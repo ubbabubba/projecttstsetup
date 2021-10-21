1bc68257dbae825d38e86e45b79672f49829a1c1
John Miller <jmiller@depaul.edu>
2021-10-21 15:16:56 -0500

    Initial commit

---
+++ tests/test_transportation.py
@@ -0,0 +1,10 @@
+from car import transportation
+
+def test_car():
+    car_object = transportation.Car()
+    assert True
+
+
+def test_motorcycle():
+    moto_object = transportation.Motorcycle()
+    assert True
